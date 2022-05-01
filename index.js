5c04f829864435bba52b764fdda5680a90f9ec63
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-01 23:52:48 +0600

    add file for oner inventory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 var cors = require('cors');
 const port = process.env.PORT || 5000
@@ -8,8 +8,6 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-//carWorld
-//hA0zc7zXWFC1iAhD
 
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aqjd3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -27,15 +25,18 @@ async function run() {
          res.send(inventories);
   })
 
-    //get one inventory
-    
-    app.get("/inventories/:id",async(req,res)=>{
-      const id = req.params.id
-      const query = {_id:ObjectId(id)};
-      const getOneInventory = await inventoryCollection.findOne(query);
-      res.send(getOneInventory )
-  })
+        //get one inventory
+        
+        app.get("/inventory/:Id",async(req,res)=>{
+          const Id = req.params.Id
+          const query = {_id:ObjectId(Id)};
+          const getOneInventory = await inventoryCollection.findOne(query);
+          res.send(getOneInventory)
+      })
 
+
+
+  
     } 
     finally {
     
